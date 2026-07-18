75eac1545129c8af4d522382ca3fc03221663b20
Maksatbek <maksatbek036@gmail.com>
2026-07-18 11:47:24 +0300

    Добавить реализацию репозитория в памяти

--- domain/Store/Book.cs
+++ domain/Store/Book.cs
@@ -4,7 +4,14 @@ using System.Text;
 
 namespace Store
 {
-    internal class Book
+    public class Book
     {
+        public int Id { get; }
+        public string Title { get;  }
+        public Book(int id, string title)
+        {
+            Id = id;
+            Title = title;
+        }
     }
 }

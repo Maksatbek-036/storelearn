616718e967b7fc6a41038a50102a7adabecb0076
Maksatbek <maksatbek036@gmail.com>
2026-07-18 12:58:14 +0300

    Создат веб веб-приложения

---
+++ Infastructure/Store.Memory/BookRepository.cs
@@ -0,0 +1,18 @@
+﻿namespace Store.Memory
+{
+    public class BookRepository : IBookRepository
+    {
+        public readonly Book[] books = new Book[] { 
+        new Book(1, "The Great Gatsby"),
+        new Book(2, "To Kill a Mockingbird"),
+        new Book(3, "1984"),
+        new Book(4, "Art of programming"),
+        };
+
+        public Book[] GetAllByTitle(string titlePartial)
+        {
+            return books.Where(book => book.Title.Contains(titlePartial))
+                .ToArray();
+        }
+    }
+}
